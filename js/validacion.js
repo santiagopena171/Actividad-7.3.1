345d078c8a55bde06f8270b8f9fe18ff8dbd3318
santiagopena171 <santijoelpena@gmail.com>
2025-08-15 11:13:14 -0300

    trabajo finalizado
    Co-authored-by: SantiagoRodríguez <santiago-rodriguez2@users.noreply.github.com>
    Co-authored-by: Bruno Giacometti <chaskibum@users.noreply.github.com>
    Co-authored-by: Fernanda Díaz <feruzu@users.noreply.github.com>

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,27 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+document.getElementById("regBtn").addEventListener("click", function () {
+
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let email = document.getElementById("email").value;
+    let password1 = document.getElementById("password1").value;
+    let password2 = document.getElementById("password2").value;
+    let terminos = document.getElementById("terminos").checked;
+
+    
+    if (
+        nombre !== "" &&
+        apellido !== "" &&
+        email !== "" &&
+        password1.length >= 6 &&
+        password2 === password1 &&
+        terminos 
+        ) {
+        showAlertSuccess();
+    } else {
+        showAlertError();
+    };
+});
